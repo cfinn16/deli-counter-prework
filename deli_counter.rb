2053c0067ecbd3ea8a50062450a35bc827733400
Connor Finnegan <connorfinnegan16@gmail.com>
2018-10-12 14:24:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,11 @@
 katz_deli = []
 
 def line(katz_deli)
-  a = []
-  i = 0 
   if katz_deli.length < 1 
     puts "The line is currently empty."
   else 
+   a = []
+   i = 0
    while i < katz_deli.length
      a.push("#{i + 1}. #{katz_deli[i]},")
    end
