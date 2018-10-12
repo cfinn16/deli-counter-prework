afc40abc8c2de3f83d734e06103b5935bc2fe67c
Connor Finnegan <connorfinnegan16@gmail.com>
2018-10-12 14:31:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,7 @@ katz_deli = []
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
 
 def line(katz_deli)
   if katz_deli.length < 1 
